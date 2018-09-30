ecafccdce63c105bc65eddaeb52a33c1b07cdda1
severus889700 <severus889700@gmail.com>
2018-09-30 21:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy Holidays, ${name}!`
 }
-function happyHolidaysTo(holidays, name){
+function happyHolidayTo(holidays, name){
   return `Happy ${holidays} ${name}!`
 }
 function holidayCountdown(days, holidays){
